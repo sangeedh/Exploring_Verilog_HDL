4934059129eab4cbc4dfd3554fb8d44e4c79afa8
sangeedh <59922796+sangeedh@users.noreply.github.com>
2023-06-02 12:20:59 +0530

    Example 2: Binary Adder.v

---
+++ Chapter 1: Introduction to Digital Design and Verilog/Example 2: Binary Adder.v
@@ -0,0 +1,4 @@
+module BinaryAdder(input [3:0] a, b, output [3:0] sum);
+  assign sum = a + b;
+endmodule
+//This Verilog code represents a 4-bit binary adder. It takes two 4-bit inputs, a and b, and produces a 4-bit sum as the output.
