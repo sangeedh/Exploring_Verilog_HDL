f2316e92babceb2672d97450419f5410da1ab648
sangeedh <59922796+sangeedh@users.noreply.github.com>
2023-06-02 12:20:13 +0530

    Example 1: Hello World in Verilog.v

---
+++ Chapter 1: Introduction to Digital Design and Verilog/Example 1: Hello World in Verilog.v
@@ -0,0 +1,7 @@
+module HelloWorld;
+  initial begin
+    $display("Hello, World!");
+    $finish;
+  end
+endmodule
+//This simple Verilog code prints "Hello, World!" to the console using the $display system function.
