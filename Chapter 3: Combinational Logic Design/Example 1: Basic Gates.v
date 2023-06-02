3affd399ca5338ac88f143f8be5d2a08608e5ed6
Sangeedh <59922796+sangeedh@users.noreply.github.com>
2023-06-02 12:44:55 +0530

    Example 1: Basic Gates.v

--- Chapter 3: Combinational Logic Design/Example 1: Basic Gates.v
+++ Chapter 3: Combinational Logic Design/Example 1: Basic Gates.v
@@ -1,6 +1,7 @@
-module BasicGates(input a, b, output c, d, e, f);
+module BasicGates(input a, b, output c, d, e, f,g);
   assign c = a & b;   // AND gate
   assign d = a | b;   // OR gate
   assign e = a ^ b;   // XOR gate
   assign f = ~a;      // NOT gate
+  assign g = a~&b;    //NAND gate
 endmodule
