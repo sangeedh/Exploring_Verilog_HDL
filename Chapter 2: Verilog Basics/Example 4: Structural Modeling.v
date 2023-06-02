ca65331f523d1967bddfb6720cdc3a02797a2012
sangeedh <59922796+sangeedh@users.noreply.github.com>
2023-06-02 12:31:08 +0530

    Example 4: Structural Modeling.v

---
+++ Chapter 2: Verilog Basics/Example 4: Structural Modeling.v
@@ -0,0 +1,5 @@
+module StructuralModeling(input a, b, output c);
+  and gate1(c, a, b);
+endmodule
+//This Verilog code demonstrates structural modeling using gate-level primitives. 
+//It describes a 2-input AND gate (gate1) that connects inputs a and b to output c.
